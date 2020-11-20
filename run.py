5495f20f34e695282852c96a2d19bfbd6dc6fe8e
antonkurenkov <a.k.a1306@gmail.com>
2020-11-21 00:07:21 +0300

    fix: fix

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app import app, views
 if __name__ == '__main__':
     # website_url = 'freeqrcode.ml'
     # app.config['SERVER_NAME'] = website_url
-    app.run(debug=False)
+    app.run(debug=True)
