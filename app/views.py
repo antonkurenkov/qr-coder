25a94ca52afb4453c04462a6fa04156e37b3d12b
antonkurenkov <a.k.a1306@gmail.com>
2020-11-23 01:08:27 +0300

    return postgres for heroku

--- app/views.py
+++ app/views.py
@@ -50,7 +50,7 @@ def main_form_post():
         in_image.save(imgByteArr, format='PNG')
         imgByteArr = imgByteArr.getvalue()
         
-        # BaseConnector().insert(code=textcode, imgByteArr=imgByteArr)
+        BaseConnector().insert(code=textcode, imgByteArr=imgByteArr)
         # out_image = BaseConnector().select(code=textcode)
 
         # return render_template('error.html', error=str(out_image))
