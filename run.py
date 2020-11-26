9eb612b65d22a2f8b0ae8f10a9bc6e260772d611
antonkurenkov <a.k.a1306@gmail.com>
2020-11-26 17:35:22 +0300

    certs

--- run.py
+++ run.py
@@ -2,4 +2,4 @@
 from app import app, views
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(ssl_context=('certificate_ca.crt,', 'certificate.crt'), debug=True, host='31.31.192.197')
