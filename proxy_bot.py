8968df705b15f4b0ed52efb172b0cb39f98c4df3
antonkurenkov <a.k.a1306@gmail.com>
2020-11-29 13:16:57 +0300

    test opera

--- proxy_bot.py
+++ proxy_bot.py
@@ -160,21 +160,21 @@ class User(ProxyMiner, Solver):
         #     options=options
         # )
 
-#        opera_profile = '/home/antonkurenkov/qr-coder/opera-conf/opera'
-#        options = webdriver.ChromeOptions()
-#        options.headless = True
-#        # options.add_argument('user-data-dir=' + opera_profile)
-#        # driver = webdriver.Opera(options=options, executable_path='/home/antonkurenkov/Proj/qr-coder/operadriver-86-linux')
-#        options.add_argument("--no-sandbox")
-#        options.add_argument('--disable-dev-shm-usage')
-#        options.add_argument("--disable-extensions") 
-#        options.add_argument("--disable-gpu") 
-#        options.add_argument("start-maximized") 
-#        options.add_argument("disable-infobars") 
-         
-#        options.add_argument("--disable-setuid-sandbox")
-#        options.add_argument("--remote-debugging-port=9222")
-#        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        opera_profile = '/home/antonkurenkov/qr-coder/opera-conf/opera'
+        options = Options()
+        options.headless = True
+        # options.add_argument('user-data-dir=' + opera_profile)
+        # driver = webdriver.Opera(options=options, executable_path='/home/antonkurenkov/Proj/qr-coder/operadriver-86-linux')
+        options.add_argument("--no-sandbox")
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--start-maximized")
+        options.add_argument("--disable-infobars")
+
+        options.add_argument("--disable-setuid-sandbox")
+        options.add_argument("--remote-debugging-port=9222")
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
         self.driver = webdriver.Opera(options=options,
                                 executable_path='/home/antonkurenkov/qr-coder/operadriver-86-linux')
 
