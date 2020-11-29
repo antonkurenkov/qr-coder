2cfd949cb9af692c245702b5cf3610259173dab8
antonkurenkov <a.k.a1306@gmail.com>
2020-11-29 13:13:13 +0300

    test opera

--- proxy_bot.py
+++ proxy_bot.py
@@ -154,10 +154,11 @@ class User(ProxyMiner, Solver):
         options.add_argument("window-size=1024,768")
         options.add_argument('--no-sandbox')
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        self.driver = webdriver.Chrome(
-            executable_path="/home/antonkurenkov/qr-coder/chromedriver-86-linux",
-            options=options
-        )
+
+        # self.driver = webdriver.Chrome(
+        #     executable_path="/home/antonkurenkov/qr-coder/chromedriver-86-linux",
+        #     options=options
+        # )
 
 #        opera_profile = '/home/antonkurenkov/qr-coder/opera-conf/opera'
 #        options = webdriver.ChromeOptions()
@@ -174,8 +175,8 @@ class User(ProxyMiner, Solver):
 #        options.add_argument("--disable-setuid-sandbox")
 #        options.add_argument("--remote-debugging-port=9222")
 #        options.add_experimental_option('excludeSwitches', ['enable-logging'])
-#        self.driver = webdriver.Opera(options=options,
-#                                 executable_path='/home/antonkurenkov/qr-coder/operadriver-86-linux')
+        self.driver = webdriver.Opera(options=options,
+                                executable_path='/home/antonkurenkov/qr-coder/operadriver-86-linux')
 
     @staticmethod
     def happened(probability_coeff=100, always=False):
