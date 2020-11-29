291e6ce171eea9a5135b80e29558d4ef9eeb02f4
antonkurenkov <a.k.a1306@gmail.com>
2020-11-29 13:21:25 +0300

    return chrome

--- proxy_bot.py
+++ proxy_bot.py
@@ -137,46 +137,53 @@ class User(ProxyMiner, Solver):
         #         "proxyType": "MANUAL",
         #     }
         #
+
         options = Options()
+
         # canonic_ua = 'user-agent=Mozilla/5.0 (X11; Linux x86_64) ' \
         #              'AppleWebKit/537.36 (KHTML, like Gecko) ' \
         #              'Chrome/86.0.4240.111 Safari/537.36'
         # if random.randint(0, 100) <= 30:
         #     canonic_ua = self.useragent
         # options.add_argument(canonic_ua)
-        #
+
         options.headless = True
         #
-        # if random.randint(0, 100) >= 10:
-        #     options.add_argument('--start-maximized')
+        if random.randint(0, 100) >= 30:
+            options.add_argument('--start-maximized')
+        elif random.randint(0, 100) >= 30:
+            options.add_argument("window-size=1024,768")
+
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-gpu')
-        options.add_argument("window-size=1024,768")
+
         options.add_argument('--no-sandbox')
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-        # self.driver = webdriver.Chrome(
-        #     executable_path="/home/antonkurenkov/qr-coder/chromedriver-86-linux",
-        #     options=options
-        # )
+        self.driver = webdriver.Chrome(
+            executable_path="/home/antonkurenkov/qr-coder/chromedriver-86-linux",
+            options=options
+        )
 
-        opera_profile = '/home/antonkurenkov/qr-coder/opera-conf/opera'
-        options = Options()
-        options.headless = True
-        # options.add_argument('user-data-dir=' + opera_profile)
-        # driver = webdriver.Opera(options=options, executable_path='/home/antonkurenkov/Proj/qr-coder/operadriver-86-linux')
-        options.add_argument("--no-sandbox")
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument("--disable-extensions")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--start-maximized")
-        options.add_argument("--disable-infobars")
 
-        options.add_argument("--disable-setuid-sandbox")
-        options.add_argument("--remote-debugging-port=9222")
-        options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        self.driver = webdriver.Opera(options=options,
-                                executable_path='/home/antonkurenkov/qr-coder/operadriver-86-linux')
+
+        # opera_profile = '/home/antonkurenkov/qr-coder/opera-conf'
+        # options = Options()
+        # options.headless = True
+        # # options.add_argument('user-data-dir=' + opera_profile)
+        # # driver = webdriver.Opera(options=options, executable_path='/home/antonkurenkov/Proj/qr-coder/operadriver-86-linux')
+        # options.add_argument("--no-sandbox")
+        # options.add_argument('--disable-dev-shm-usage')
+        # options.add_argument("--disable-extensions")
+        # options.add_argument("--disable-gpu")
+        # options.add_argument("--start-maximized")
+        # options.add_argument("--disable-infobars")
+        #
+        # options.add_argument("--disable-setuid-sandbox")
+        # options.add_argument("--remote-debugging-port=9222")
+        # options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        # self.driver = webdriver.Opera(options=options,
+        #                         executable_path='/home/antonkurenkov/qr-coder/operadriver-86-linux')
 
     @staticmethod
     def happened(probability_coeff=100, always=False):
