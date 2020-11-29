d21e381a681ecb02edef61fb29409859bc1d9585
antonkurenkov <a.k.a1306@gmail.com>
2020-11-29 15:52:48 +0300

    chrome options back

--- proxy_bot.py
+++ proxy_bot.py
@@ -157,7 +157,7 @@ class User(ProxyMiner, Solver):
         # elif random.randint(0, 100) >= 30:
         #     options.add_argument("window-size=1024,768")
 
-        options.add_argument('--disable-dev-shm-usage')
+        # options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
         # options.add_argument("--disable-infobars")
